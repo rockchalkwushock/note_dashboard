f87e1b58d22a71a4d81840e4ab56523cbabd431f
Cody Brunner <rockchalkwushock@icloud.com>
2017-05-19 09:59:03 -0500

    refactor(api/mocks): Updated mockRoute method [skip ci]
    Refactored method to accept multiple endpoints & return mocked requests based on the requirements of
    that endpoint. This will be more efficient as the test suite grows.

--- api/__mocks__/utils/index.js
+++ api/__mocks__/utils/index.js
@@ -2,14 +2,26 @@ import request from 'supertest';
 import server from '../../index';
 
 /**
- * mockRoute(arg1, arg2)
+ * mockRoute(arg1, arg2, arg3)
  *
  * Helper function for mocking endpoints
  * with fake user data.
  *
- * @param {String} route The endpoint to be mocked.
- * @param {Object} data Generated fake user data.
- * @returns {Object} res Response object.
+ * @param {String} endpoint - The endpoint being tested.
+ * @param {String} route - The full endpoint for supertest to make a request on.
+ * @param {Object} data - The mock req.data object.
+ * @returns {Object} res - Response Object.
  */
- export const mockRoute = (route, data) => request(server).post(route).send(data);
-
+export const mockRoute = (endpoint, route, data) => {
+  switch (endpoint) {
+    case '/sign-up': return request(server).post(route).send(data);
+    case '/sign-in': return null;
+    case '/check-token': return null;
+    case '/new-note': return null;
+    case '/edit-note': return null;
+    case '/delete-note': return null;
+    case '/get-note': return null;
+    case '/get-notes': return null;
+    default: return 'Oops you screwed something up!';
+  }
+}
