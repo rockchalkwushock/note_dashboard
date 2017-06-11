e4447a2b2794cb9873809cb6a46265da55cfb696
Cody Brunner <rockchalkwushock@icloud.com>
2017-06-10 21:20:51 -0500

    feat(src/Routes): Added PrivateRoute & integrated into app [skip ci]

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import { PrivateRoute } from './components'
 import { Dashboard, Login, Signup } from './pages'
 
 /**
- * TODO:
- * 1. Make and add <PrivateRoute />
- *
  * NOTE:
- * path='/' will be a private route as will
- * anything behind this route that is not '/login'
- * or '/signup'
+ * You MUST create '/signup' & '/login' views now.
+ * Without and authenticated user you cannot reach '/'.
  */
 
 export default () =>
   <Switch>
-    <Route exact path="/" component={Dashboard} />
+    <PrivateRoute exact path="/" component={Dashboard} />
     <Route exact path="/signin" component={Signup} />
     <Route exact path="/signup" component={Login} />
   </Switch>
